a1c9f9c3a9fbea3f35e8d03c44be3d1244fef093
szabee994 <szabee94@gmail.com>
2015-10-21 15:59:07 +0200

    dim added

--- app/src/main/java/com/awt/supark/MainActivity.java
+++ app/src/main/java/com/awt/supark/MainActivity.java
@@ -3,15 +3,24 @@ package com.awt.supark;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    public void onBackPressed() {
+        //Display alert message when back button has been pressed
+        Log.i("SuPark", "Back pressed.");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +30,14 @@ public class MainActivity extends AppCompatActivity {
         // Declaring the animations
         final Animation anim_zoom_in = AnimationUtils.loadAnimation(this, R.anim.zoom_in);
         final Animation anim_zoom_out = AnimationUtils.loadAnimation(this, R.anim.zoom_out);
+        final Animation anim_fade_in = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         final Animation anim_fade_out = AnimationUtils.loadAnimation(this, R.anim.fade_out);
 
         // Declaring the ui elements
         final ImageButton btnPark = (ImageButton) findViewById(R.id.buttonPark);
-        final FrameLayout parkingFrame = (FrameLayout) findViewById(R.id.parkingFrame);
+        final RelativeLayout backDimmer = (RelativeLayout) findViewById(R.id.back_dimmer);
 
-        // Event listener which will listen to the circle button's touch events
+        // Setting up a listener to track the touch/release events for the parking button
         btnPark.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -37,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 if (motionEvent.getAction() == android.view.MotionEvent.ACTION_DOWN) {
                     // Do the animation
                     view.startAnimation(anim_zoom_out);
-                    anim_zoom_in.setFillAfter(true);
+                    anim_zoom_out.setFillAfter(true);
                 }
 
                 // If the user releases the button zooming out animation starts.
@@ -45,35 +55,26 @@ public class MainActivity extends AppCompatActivity {
                 else if (motionEvent.getAction() == android.view.MotionEvent.ACTION_UP) {
                     // Do the animation
                     view.startAnimation(anim_zoom_in);
-                    anim_zoom_out.setFillAfter(true);
-
-                    // Setting up a listener which will track the animation,
-                    // and continues the program after the animation ended.
-                    anim_zoom_out.setAnimationListener(new Animation.AnimationListener() {
-                        @Override
-                        public void onAnimationStart(Animation animation) {
-
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animation animation) {
-                            // Continue with fading out other UI elements
-                            //parkingFrame.startAnimation(anim_fade_out);
-                            //anim_fade_out.setFillAfter(true);
-
-                            // Continue with starting the next intent
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animation animation) {
-
-                        }
-                    });
+                    anim_zoom_in.setFillAfter(true);
                 }
 
                 // This is required for some reasons.
                 return false;
             }
         });
+
+        // Setting up a listener to track the click events for the parking button
+        btnPark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // If the user clicked the parking button we should dim the background
+                // by setting the dimming layer visible.
+                backDimmer.setVisibility(View.VISIBLE);
+                backDimmer.startAnimation(anim_fade_in);
+            }
+        });
+
+
     }
+
 }
