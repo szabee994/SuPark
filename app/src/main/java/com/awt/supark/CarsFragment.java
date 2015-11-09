969504a93788593b19ca25c42f9eb7652f79acd7
szabee994 <szabee94@gmail.com>
2015-11-09 14:28:26 +0100

    commit for mark

--- app/src/main/java/com/awt/supark/CarsFragment.java
+++ app/src/main/java/com/awt/supark/CarsFragment.java
@@ -2,26 +2,48 @@ package com.awt.supark;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * Created by Mark on 2015.10.25..
- */
+
+import com.awt.supark.R;
+//import com.awt.supark.adapter.CarListAdapter;
+//import com.awt.supark.model.Car;
+import com.google.android.gms.common.images.ImageManager;
+
+import java.util.ArrayList;
+
+
 public class CarsFragment extends Fragment {
 
     View view;
-    Button btn1;
+    ImageButton btn1;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.cars_layout, container, false);
-        Log.d("CarsFragment", "Cars activity started");
+
+        btn1 = (ImageButton) view.findViewById(R.id.buttonAdd);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                foo();
+            }
+        });
+
+
         return view;
+    }
+
+    public void foo() {
+        Toast.makeText(getActivity(), "ADD", Toast.LENGTH_LONG).show();
     }
 }
